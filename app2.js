fb24b3148716076b2124e9837b01f119d5053f18
AnasBilal26 <anasbilalbhai@gmail.com>
2024-05-25 17:10:59 +0500

    html css and javascript file

---
+++ app2.js
@@ -0,0 +1,32 @@
+var menuicon =document.querySelector(".menuicon")
+var ffll =document.querySelector(".fullscreen")
+var full =document.querySelector(" .fullscreensec")
+
+menuicon.addEventListener("click",function(){
+full.style.right =0;
+ffll.style.right =0;
+});
+var full =document.querySelector(" .fullscreensec")
+var cross = document.querySelector (".cross")
+var ffll =document.querySelector(".fullscreen")
+
+
+cross.addEventListener("click",function(){
+    full.style.right ="-100%";
+    ffll.style.right ="-100%";
+    });
+    
+cross.addEventListener("click",function(){
+    full.style.right ="-100%";
+    ffll.style.right ="-100%";
+    });
+    var loader =document.querySelector(".loader")
+    setTimeout(function(){
+
+loader.style.top ="-100%"
+
+
+
+
+
+    },3000)
